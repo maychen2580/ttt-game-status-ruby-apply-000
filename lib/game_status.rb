9f36bdb7e40b13e6af7e0e9f0b773d146904369d
delightful-dispatch-9896 <maychen2580@hotmail.com>
2020-08-03 02:52:53 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,7 +15,7 @@ WIN_COMBINATIONS = [
 [2,4,6]]
 
 def won?(board)
-  WIN_COMBINATIONS.detect do |combo|
+  WIN_COMBINATIONS.detect do |win_combo|
     board[combo[0]] == board[combo[1]] &&
     board[combo[1]] == board[combo[2]] &&
     position_taken?(board, combo[0])
