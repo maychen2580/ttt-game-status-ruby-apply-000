7507e9d25f457a961a581a5ed135eafbe69baebf
delightful-dispatch-9896 <maychen2580@hotmail.com>
2020-08-03 02:47:10 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,13 +21,13 @@ end
 end
 
 def won?(board)
-  
-  
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
+  end
+end
 
-      
-  
-
-  
   def full?(board)
     if board.include?(" ") || board.include?("")
       false
